87264cfa0fe84782b2379fe347112bd78a3400c5
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2023-12-15 17:47:54 +0200

    Revert "Revert "auth backend""

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,156 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/HomeScreen.dart';
+import 'package:flutter_application_1/PreHomeScreen.dart';
+import 'package:flutter_application_1/signin.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String title;
 
   MyHomePage({required this.title});
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  String _errorMessage = ''; // Added error message state
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         backgroundColor: Color.fromARGB(255, 227, 183, 121),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFFEAD5B7), Color(0xFFFFFFFF)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFEAD5B7), Color(0xFFFFFFFF)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Image.asset(
-                  'assets/Group_2.png', // Update the path based on your project structure
-                  height: 150.0,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Welcome Back',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  height: 1.25,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.normal,
-                  color: Color(0xFF000000),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.black),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/Group_2.png',
+                    width: 150.0,
+                    height: 150.0,
                   ),
                 ),
-                style: TextStyle(color: Colors.black),
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.black),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+                SizedBox(height: 16.0),
+                Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    height: 1.25,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.normal,
+                    color: Color(0xFF000000),
                   ),
                 ),
-                style: TextStyle(color: Colors.black),
-                obscureText: true,
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      // Add logic for forgot password
-                    },
-                    child: Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        fontSize: 15,
-                        height: 1.59,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF000000),
-                        letterSpacing: 0.08,
-                      ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.black),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFF9CF93),
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.black),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeScreen()),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.arrow_forward,
-                        color: Colors.white,
-                      ),
-                      iconSize: 40.0,
-                      splashRadius: 35.0,
-                      padding: EdgeInsets.all(0.0),
-                      alignment: Alignment.center,
-                      tooltip: 'Create Account',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
-                  )
-                ],
-              ),
-              SizedBox(height: 16.0),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TextButton(
                       onPressed: () {
-                        // No navigation logic here
+                        // Add logic for forgot password
                       },
                       child: Text(
-                        'Sign up',
-                        style: TextStyle(
-                          fontSize: 24,
-                          height: 1.2,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.normal,
-                          color: Color(0xFF000000),
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        // No navigation logic here
-                      },
-                      child: Text(
-                        'Don’t have an account?',
+                        'Forgot Password?',
                         style: TextStyle(
                           fontSize: 15,
                           height: 1.59,
@@ -161,13 +108,107 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFF9CF93),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          _signInWithEmailAndPassword();
+                        },
+                        icon: Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                        iconSize: 40.0,
+                        splashRadius: 35.0,
+                        padding: EdgeInsets.all(0.0),
+                        alignment: Alignment.center,
+                        tooltip: 'Sign In',
+                      ),
+                    )
                   ],
                 ),
-              ),
-            ],
+                SizedBox(height: 16.0),
+                // Display error message
+                if (_errorMessage.isNotEmpty)
+                  Text(
+                    _errorMessage,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignInPage()),
+                          );
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                            fontSize: 24,
+                            height: 1.2,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.normal,
+                            color: Color(0xFF000000),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignInPage()),
+                          );
+                        },
+                        child: Text(
+                          'Don’t have an account?',
+                          style: TextStyle(
+                            fontSize: 15,
+                            height: 1.59,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF000000),
+                            letterSpacing: 0.08,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  // Function to sign in with email and password
+  void _signInWithEmailAndPassword() {
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+            email: _emailController.text, password: _passwordController.text)
+        .then((value) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    }).catchError((error) {
+      // Handle sign-in errors and display error message
+      setState(() {
+        _errorMessage = 'Invalid email or password. Please try again.';
+      });
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/signin.dart';
+import 'package:flutter_application_1/Browse.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/splashScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SplashScreen(),
