7576f43a61762184f79272099adb278e86dbaf58
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2023-12-19 16:49:23 +0200

    Update ChoosePaymentPage.dart
    added an import for the nav bar

--- lib/ChoosePaymentPage.dart
+++ lib/ChoosePaymentPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/HomeScreen.dart';
+import 'package:flutter_application_1/NavBar.dart';
 import 'package:flutter_application_1/Sell_lese.dart';
 import 'package:flutter_application_1/Visa.dart';
 
