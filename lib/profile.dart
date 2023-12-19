7470a9847b42d1cddc2a3268b4e7071b1fc29518
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-12-19 14:20:25 +0200

    ProfilePage and pictures

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/LoginPage.dart';
 import 'package:flutter_application_1/NavBar.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,10 +10,6 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   int currentIndex = 0;
-  // Define controllers for the editable fields
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +32,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   children: [
                     CircleAvatar(
                       radius: 30,
-                      backgroundImage: AssetImage('assets/profile_picture.jpg'), // Replace with the actual path
+                      backgroundImage: AssetImage('assets/profile_picture.jpg'),
                     ),
                     SizedBox(width: 16.0),
                     Column(
@@ -46,7 +44,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         SizedBox(height: 8.0),
                         ElevatedButton(
-                          onPressed: () {}, // Add your action here
+                          onPressed: () {},
                           style: ElevatedButton.styleFrom(
                             primary: Colors.white,
                             shape: RoundedRectangleBorder(
@@ -75,10 +73,28 @@ class _ProfilePageState extends State<ProfilePage> {
                   ],
                 ),
               ),
-              SizedBox(height: 100.0),
+              SizedBox(height: 70.0),
               buildHelpAndSupportRow(),
-              SizedBox(height: 60.0),
+              SizedBox(height: 40.0),
               buildAboutUsRow(),
+
+              SizedBox(height: 50.0),
+             ElevatedButton(
+              onPressed: () {
+                _logout();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFF9CF93),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Text(
+                'Logout',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
+
             ],
           ),
         ),
@@ -94,17 +110,8 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  // int _currentIndex = 2;
-
-  // BottomNavigationBarItem buildBottomNavigationBarItem(
-  //     IconData icon, String label, int index) {
-  //   return BottomNavigationBarItem(
-  //     icon: Icon(icon),
-  //     label: label,
-  //   );
-  // }
-
-  Widget buildProfileInfoRow(String label, String value, {double labelFontSize = 16.0, double valueFontSize = 16.0}) {
+  Widget buildProfileInfoRow(String label, String value,
+      {double labelFontSize = 16.0, double valueFontSize = 16.0}) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
@@ -116,7 +123,7 @@ class _ProfilePageState extends State<ProfilePage> {
               Text(label, style: TextStyle(fontSize: labelFontSize, color: Colors.white)),
               SizedBox(height: 8.0),
               ElevatedButton(
-                onPressed: () {}, // Add your action here
+                onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   primary: Color.fromARGB(255, 255, 255, 255),
                   shape: RoundedRectangleBorder(
@@ -125,7 +132,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 child: Text(
                   value,
-                  style: TextStyle(fontSize: valueFontSize, color: Colors.black), // Set text color to black
+                  style: TextStyle(fontSize: valueFontSize, color: Colors.black),
                 ),
               ),
             ],
@@ -133,14 +140,13 @@ class _ProfilePageState extends State<ProfilePage> {
           IconButton(
             icon: Icon(Icons.edit),
             onPressed: () {
-              // Handle edit functionality here
               _showEditDialog(label, value);
             },
           ),
         ],
       ),
     );
-  } 
+  }
 
   Widget buildHelpAndSupportRow() {
     return Row(
@@ -182,63 +188,64 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  // Function to show the edit dialog
- Future<void> _showEditDialog(String label, String value) async {
-  TextEditingController? controller;
+  Future<void> _showEditDialog(String label, String value) async {
+    TextEditingController? controller;
 
-  switch (label) {
-    case 'Name':
-      controller = nameController;
-      break;
-    case 'Email':
-      controller = emailController;
-      break;
-    case 'Phone':
-      controller = phoneController;
-      break;
-  }
+    switch (label) {
+      case 'Name':
+        controller = TextEditingController();
+        break;
+      case 'Email':
+        controller = TextEditingController();
+        break;
+      case 'Phone':
+        controller = TextEditingController();
+        break;
+    }
 
-  // Initialize the controller if it hasn't been assigned
-  controller ??= TextEditingController();
+    controller ??= TextEditingController();
+    controller.text = value;
 
-  // Set the initial text in the text field to the current value
-  controller.text = value;
-
-  // Show the dialog
-  await showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Edit $label'),
-        content: SingleChildScrollView(
-          child: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              hintText: 'Enter $label',
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit $label'),
+          content: SingleChildScrollView(
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: 'Enter $label',
+              ),
             ),
           ),
-        ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0), // Adjust the padding as needed
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context); // Close the dialog
-            },
-            child: Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              // Save the edited value
-              String editedValue = controller?.text ?? '';
-              // TODO: Add logic to save the edited value (e.g., update it in a database)
-              print('Edited $label: $editedValue');
-              Navigator.pop(context); // Close the dialog
-            },
-            child: Text('Save'),
-          ),
-        ],
-      );
-    },
-  );
-}
+          contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                String editedValue = controller?.text ?? '';
+                print('Edited $label: $editedValue');
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _logout() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage(title: 'Login')),
+      (route) => false,
+    );
+  }
 }
