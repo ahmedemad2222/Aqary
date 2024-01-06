0226ed1336be728c3296df7ce48e1ed8d02e02c5
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2024-01-06 21:06:21 +0200

    main edits for dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Browse.dart';
+import 'package:flutter_application_1/ThemeProvider.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/splashScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
-  ));
+
+   runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            theme: context.watch<ThemeProvider>().themeData,
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          );
+        },
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: context.watch<ThemeProvider>().themeData,
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
