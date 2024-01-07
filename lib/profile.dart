8184f66d579ed341377bd8318d5eb4d9b0904a64
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2024-01-07 21:55:51 +0200

    profile pic now apears in the profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,16 +5,21 @@ import 'package:flutter_application_1/LoginPage.dart';
 import 'package:flutter_application_1/NavBar.dart';
 import 'package:flutter_application_1/ThemeProvider.dart';
 import 'package:provider/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:ui' as ui;
 
 class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
   int currentIndex = 2;
-  late String email;
-  late String name;
+  late String email = '';
+  late String name = '';
+  late String profileImageUrl = '';
   late TextEditingController nameController;
 
   @override
@@ -41,6 +46,10 @@ class _ProfilePageState extends State<ProfilePage> {
           setState(() {
             email = data['email'] ?? 'No Email';
             name = data['name'] ?? 'No Name';
+            List<String>? imageUrls = List<String>.from(data['imageUrls']);
+            profileImageUrl = (imageUrls != null && imageUrls.isNotEmpty)
+                ? imageUrls[0]
+                : 'USER_PROFILE_IMAGE';
             nameController.text = name;
           });
         } else {
@@ -48,6 +57,7 @@ class _ProfilePageState extends State<ProfilePage> {
           setState(() {
             email = 'No Email';
             name = 'No Name';
+            profileImageUrl = 'USER_PROFILE_IMAGE';
           });
         }
       } else {
@@ -55,6 +65,7 @@ class _ProfilePageState extends State<ProfilePage> {
         setState(() {
           email = 'No Email';
           name = 'No Name';
+          profileImageUrl = 'USER_PROFILE_IMAGE';
         });
       }
     } catch (e, stackTrace) {
@@ -98,63 +109,51 @@ class _ProfilePageState extends State<ProfilePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Color.fromARGB(255, 227, 183, 121),
+        title: const Text('Profile'),
+        backgroundColor: const Color.fromARGB(255, 227, 183, 121),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             buildDarkLightToggle(isDarkMode),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              color: isDarkMode
-                  ? Colors.black
-                  : Color.fromARGB(255, 227, 183, 121),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/profile_picture.jpg'),
-                  ),
-                  SizedBox(width: 16.0),
-                ],
-              ),
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(profileImageUrl),
             ),
+            const SizedBox(height: 16.0),
             Container(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               color: isDarkMode
                   ? Colors.black
-                  : Color.fromARGB(255, 227, 183, 121),
+                  : const Color.fromARGB(255, 227, 183, 121),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  buildProfileInfoRow(
-                      'Name', name, isDarkMode,
+                  buildProfileInfoRow('Name', name, isDarkMode,
                       labelFontSize: 20.0, valueFontSize: 18.0),
-                  buildProfileInfoRow(
-                      'Email', email, isDarkMode,
+                  buildProfileInfoRow('Email', email, isDarkMode,
                       labelFontSize: 20.0, valueFontSize: 18.0),
                 ],
               ),
             ),
-            SizedBox(height: 70.0),
+            const SizedBox(height: 70.0),
             buildHelpAndSupportRow(isDarkMode),
-            SizedBox(height: 40.0),
+            const SizedBox(height: 40.0),
             buildAboutUsRow(isDarkMode),
-            SizedBox(height: 50.0),
+            const SizedBox(height: 50.0),
             ElevatedButton(
               onPressed: () {
                 _logout();
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFF9CF93),
+                backgroundColor: const Color(0xFFF9CF93),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              child: Text(
+              child: const Text(
                 'Logout',
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
               ),
@@ -184,16 +183,17 @@ class _ProfilePageState extends State<ProfilePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(label,
-                  style: TextStyle(fontSize: labelFontSize, color: Colors.white)),
-              SizedBox(height: 8.0),
+                  style:
+                      TextStyle(fontSize: labelFontSize, color: Colors.white)),
+              const SizedBox(height: 8.0),
               ElevatedButton(
                 onPressed: () {
                   _showEditDialog(label, value);
                 },
                 style: ElevatedButton.styleFrom(
-                  primary: isDarkMode
+                  backgroundColor: isDarkMode
                       ? Colors.grey[800]
-                      : Color.fromARGB(255, 255, 255, 255),
+                      : const Color.fromARGB(255, 255, 255, 255),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
@@ -208,7 +208,7 @@ class _ProfilePageState extends State<ProfilePage> {
             ],
           ),
           IconButton(
-            icon: Icon(Icons.edit),
+            icon: const Icon(Icons.edit),
             onPressed: () {
               _showEditDialog(label, value);
             },
@@ -229,9 +229,9 @@ class _ProfilePageState extends State<ProfilePage> {
                 size: 30.0,
                 color: isDarkMode
                     ? Colors.grey[800]
-                    : Color.fromARGB(255, 219, 177, 118)),
-            SizedBox(width: 16.0),
-            Text(
+                    : const Color.fromARGB(255, 219, 177, 118)),
+            const SizedBox(width: 16.0),
+            const Text(
               'Help & Support',
               style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
@@ -241,7 +241,7 @@ class _ProfilePageState extends State<ProfilePage> {
             size: 30.0,
             color: isDarkMode
                 ? Colors.grey[800]
-                : Color.fromARGB(255, 219, 177, 118)),
+                : const Color.fromARGB(255, 219, 177, 118)),
       ],
     );
   }
@@ -257,9 +257,9 @@ class _ProfilePageState extends State<ProfilePage> {
                 size: 30.0,
                 color: isDarkMode
                     ? Colors.grey[800]
-                    : Color.fromARGB(255, 219, 177, 118)),
-            SizedBox(width: 16.0),
-            Text(
+                    : const Color.fromARGB(255, 219, 177, 118)),
+            const SizedBox(width: 16.0),
+            const Text(
               'About Us',
               style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
@@ -269,7 +269,7 @@ class _ProfilePageState extends State<ProfilePage> {
             size: 30.0,
             color: isDarkMode
                 ? Colors.grey[800]
-                : Color.fromARGB(255, 219, 177, 118)),
+                : const Color.fromARGB(255, 219, 177, 118)),
       ],
     );
   }
@@ -291,13 +291,14 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
           ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Text('Cancel'),
+              child: const Text('Cancel'),
             ),
             TextButton(
               onPressed: () async {
@@ -309,7 +310,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
                 Navigator.pop(context);
               },
-              child: Text('Save'),
+              child: const Text('Save'),
             ),
           ],
         );
@@ -340,7 +341,7 @@ class _ProfilePageState extends State<ProfilePage> {
   void _logout() {
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(builder: (context) => MyHomePage(title: 'Login')),
+      MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Login')),
       (route) => false,
     );
   }
